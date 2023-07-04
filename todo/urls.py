5096dc684598bb86d8ce79e88e2f8de81d4a0d3f
LEE MUN HONG <leemunhong78@gmail.com>
2023-07-05 06:18:48 +0800

    added url for new api: mark_todo_item_completed

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 
-from .views import TodoList, TodoDetail
+from .views import TodoList, TodoDetail, mark_todo_item_completed
 
 apiURL = [
     path('todo', TodoList.as_view(), name='todolist'),
     path('todo/<uuid:pk>', TodoDetail.as_view(), name='tododetail'),
+    path('todo/mark_completed/<uuid:pk>', mark_todo_item_completed, name='mark_todo_item_completed'),
+
 ]
 
 urlpatterns = [
