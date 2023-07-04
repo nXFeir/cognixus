9f25107859c5288442bc03b24c3eb6098719c80f
LEE MUN HONG <leemunhong78@gmail.com>
2023-07-05 06:17:16 +0800

    added new api: mark_todo_item_completed

--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,14 @@
+from django.http import QueryDict
 from django.shortcuts import get_object_or_404
-from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from todo.models import TodoItem
 
 from .serializers import TodoSerializer
-from todo.models import TodoItem
-from django.http import QueryDict
+
 
 class TodoList(generics.ListCreateAPIView):
     """
@@ -39,3 +43,11 @@ class TodoDetail(generics.RetrieveUpdateDestroyAPIView):
     
     def put(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
+    
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def mark_todo_item_completed(request, pk):
+    todo = get_object_or_404(TodoItem, uuid=pk, user=request.user)
+    todo.is_completed = True
+    todo.save()
+    return Response(TodoSerializer(todo).data, 200)
